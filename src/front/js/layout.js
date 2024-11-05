f47d6f4b4b4ef053f59dfa8a05ea223bd1c49fc1
Alerugg <134484194+Alerugg@users.noreply.github.com>
2024-11-05 21:52:58 +0000

    hecha view de user profile y navegacion de icono del navbar a ella

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -9,6 +9,7 @@ import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 import { Login } from "./pages/login";
+import { UserProfile } from "./pages/myProfile";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
@@ -30,6 +31,7 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Login />} path="/login" />
                         <Route element={<HomeUserLogued />} path="/user_logued" />
+                        <Route element={<UserProfile/>} path="/profile" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
